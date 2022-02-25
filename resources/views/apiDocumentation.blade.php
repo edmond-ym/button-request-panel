9694b00eace3bb92e0b9d2a831a85d746bb99ffe
Edmond <edmond@inchoatae.com>
2022-02-26 03:18:58 +0800

    device api docs

--- resources/views/apiDocumentation.blade.php
+++ resources/views/apiDocumentation.blade.php
@@ -7,7 +7,22 @@
 <body>
   <rapi-doc
     spec-url="{{asset('apiDoc/data.yaml')}}"
-    theme = "light"
+    theme = "graynav"
+    render-style = "read"
+    id = "thedoc"
+
+    primary-color="#f54c47"
+    nav-color="#3e4b54"
+   
+    allow-authentication = "true" 
+    allow-server-selection = "false"	
+    
+    show-info = "true"
+    show-header = "false"
+    schema-style="table"
+
+    
+
   > </rapi-doc>
 </body>
 </html>
