46e0426d52a609221c565c1d81dfa8e19a06e04f
Edmond <edmond@inchoatae.com>
2022-02-26 23:29:51 +0800

    Login Page Amend
    Add Register Link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,18 +31,27 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
+            
 
             <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 " href="{{ route('register') }}">
+                    {{ __('New User?') }}
+                </a>
+                &nbsp;&nbsp;&nbsp;
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 " href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+                
+                
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
+                
             </div>
+            
+            
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
