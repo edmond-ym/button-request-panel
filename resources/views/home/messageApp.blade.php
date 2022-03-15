ec2adebcce1f3f3ce76add9aa92f65fbee6bba0b
Edmond <edmond@inchoatae.com>
2022-03-16 06:14:34 +0800

    Home.MessageApp

--- resources/views/home/messageApp.blade.php
+++ resources/views/home/messageApp.blade.php
@@ -4,20 +4,17 @@
             <div class="row h-100">
                 <div class="col-lg-7 my-auto">
                     <div class="mx-auto header-content">
-                        <h1 class="mb-5">Message App allows you manage the App effectively in your mobile phone</h1>
-                        <a class="btn btn-outline-warning btn-xl" role="button" href="https://button-request.web.app/">Link for PWA APP</a>
+                        <h1 class="mb-5">Message App allows you manage the App effectively in your mobile phone</h1><a class="btn btn-outline-warning btn-xl" role="button" href="#download">Link for PWA APP</a>
                     </div>
                 </div>
                 <div class="col-lg-5 my-auto">
                     <div class="device-container">
                         <div class="device-mockup iphone6_plus portrait white">
-                            <div class="device" style="background-image:url('assets/img/iphone_6_plus_white_port.png');">
-                                <div class="screen"><img class="img-fluid" src="assets/img/demo-screen-1.jpg"></div>
+                            <div class="device" style="background: url(&quot;assets/img/82A69E14-0E04-497B-B23A-0F94BEA71A49_1_105_c_iphone12black_portrait.png&quot;) no-repeat;background-size: 100%;">
                                 <div class="button"></div>
                             </div>
                         </div>
                     </div>
-                    <div class="iphone-mockup"></div>
                 </div>
             </div>
         </div>
@@ -33,9 +30,7 @@
                 <div class="col-lg-4 my-auto">
                     <div class="device-container">
                         <div class="device-mockup iphone6_plus portrait white">
-                            <div class="device" style="background-image:url('assets/img/iphone_6_plus_white_port.png');">
-                                <div class="screen"><img class="img-fluid" src="assets/img/demo-screen-1.jpg"></div>
-                            </div>
+                            <div class="device" style="background: url(&quot;assets/img/6506AD32-3C52-4C4E-A46A-D6F69D6C6993_1_105_c_iphone12black_portrait.png&quot;) no-repeat;background-size: 100%;"></div>
                         </div>
                     </div>
                 </div>
@@ -49,15 +44,36 @@
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="feature-item"><i class="icon-key text-primary"></i>
-                                    <h3>Safety Access</h3>
-                                    <p class="text-muted">100 Characters Long Random One-Time Usable Key as the access credential instead of the traditional email - password way of credential for access.&nbsp;</p>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
                                 <div class="feature-item"><i class="icon-pin text-primary"></i>
                                     <h3>Message Pinnable</h3>
                                     <p class="text-muted">Pin the message you deem important</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 my-auto">
+                    <div class="device-container">
+                        <div class="device-mockup iphone6_plus portrait white">
+                            <div class="device" style="background: url(&quot;assets/img/393A813C-1411-4FCD-8CF8-A87E5D39CBE0_1_105_c_iphone12black_portrait.png&quot;) no-repeat;background-size: 100%;"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 my-auto">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="feature-item"><i class="fas fa-user-shield text-primary"></i>
+                                    <h3>Safety Access</h3>
+                                    <p class="text-muted">100 Characters Long Random One-Time Usable Key as the access credential instead of the traditional email - password way of credential for access. The random key is not transferable.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="feature-item"><i class="fas fa-trash text-primary"></i>
+                                    <h3>Key Revokable</h3>
+                                    <p class="text-muted">You may revoke the corresponding access key in the console anytime.</p>
                                 </div>
                             </div>
                         </div>
