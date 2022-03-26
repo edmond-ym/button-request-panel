b567b4a42fea085a2b313713855e45bcdbdbebad
Edmond <edmond@inchoatae.com>
2022-03-27 02:37:52 +0800

    Create APIException.php

---
+++ app/Exceptions/APIException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class APIException extends Exception
+{
+    /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        //
+    }
+ 
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    /*public function render($request)
+    {
+        
+    }*/
+    
+}
